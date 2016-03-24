02089cbc0bec3084cc360bbf6a643be9ad5d0dd9
WSiaB <johnl@kirkersoft.com>
2016-03-23 18:59:03 -0700

    Removed extra files.

--- webdesign/creator/index_files/openWin.js
+++
@@ -1,9 +0,0 @@
-<!-- hide from non-JavaScript browsers
-	
-function openWin(url, name) {
-		openNew = window.open(url, name, 'width=550,height=400,resize=yes,resizable=yes,scroll=yes,scrollbars=yes,location=yes,toolbar=yes,menubar=yes,status=yes,left=20,top=20');
-		openNew.focus();
- 	}
-	  
-// end Hide-->
-	
